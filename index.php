eb2b2824847d735ede45dbca6594739f24762433
seantots <sealtielnabor@gmail.com>
2024-10-05 20:05:39 +0800

    Pers

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Team Rambo</title>
+</head>
+<body>
+    <?php
+    $fname1 = "Dean";
+    $fname2 = "Gab";
+    $fname3 = "Seal";
+    $fname4 = "Arvin";
+
+    $lname1 = "Mosquite";
+    $lname2 = "Lebrilla";
+    $lname3 = "Nabooor";
+    $lname4 = "Gabwang";
+
+    $age1 = "16";
+    $age2 = "17";
+    $age3 = "102";
+    $age4 = "900";
+
+    $address1 = "Antips";
+    $address2 = "ValleyG";
+    $address3 = "Parula";
+    $address4 = "Pantokmol";
+
+    $contactN1 = "123";
+    $contactN2 = "456";
+    $contactN3 = "789";
+    $contactN4 = "000";
+    ?>
+    <p><?=$fname1?></p>
+    <p><?=$lname1?></p>
+    <p><?=$age1?></p>
+    <p><?=$address1?></p>
+    <p><?=$contactN1?></p>
+
+    <p><?=$fname2?></p>
+    <p><?=$lname2?></p>
+    <p><?=$age2?></p>
+    <p><?=$address2?></p>
+    <p><?=$contactN2?></p>
+
+    <p><?=$fname3?></p>
+    <p><?=$lname3?></p>
+    <p><?=$age3?></p>
+    <p><?=$address3?></p>
+    <p><?=$contactN3?></p>
+
+    <p><?=$fname4?></p>
+    <p><?=$lname4?></p>
+    <p><?=$age4?></p>
+    <p><?=$address4?></p>
+    <p><?=$contactN4?></p>
+    
+</body>
+</html>
